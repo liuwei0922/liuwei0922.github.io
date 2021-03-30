671aa1631f29cfad1a6b9c9402bd0daded13ea0a
liuwei0922 <qinmoxiao@qq.com>
2021-03-30 19:20:39 +0800

    updatefile time:date+%F

--- set.sh
+++ set.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+time='date+%F'
 rm -rf docs
 hugo -d docs
 cp googlec86aa04042151a36.html docs/
 cp CNAME docs/
-git commit -am "updatefile time:'date+%F'"
+git add .
+git commit -am "updatefile time:$time"
 git push origin master
